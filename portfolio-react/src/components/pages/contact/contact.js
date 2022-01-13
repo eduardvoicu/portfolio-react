6caa9bd874198a1c16d34e914a16a835948eeb7d
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-13 08:11:04 -0600

    added return function to contact.js file

--- portfolio-react/src/components/pages/contact/contact.js
+++ portfolio-react/src/components/pages/contact/contact.js
@@ -46,3 +46,63 @@ function Contact() {
     setMessage("");
     setEmail("");
   };
+
+  return (
+    <div>
+      <h1>Contact Page</h1>
+      <div className="formCont">
+        <div className="formStyle">
+          <Form>
+            <Form.Group className="formSpace">
+              <Form.Label style={{ color: "white" }}>Name:</Form.Label>
+              <Form.Control
+                value={FullName}
+                name="FullName"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="Full Name"
+              />
+            </Form.Group>
+            <Form.Group className="formSpace">
+              <Form.Label style={{ color: "white" }}>Email Address:</Form.Label>
+              <Form.Control
+                value={email}
+                name="email"
+                onChange={handleInputChange}
+                type="email"
+                placeholder="Email Address"
+              />
+            </Form.Group>
+            <Form.Group className="formSpace">
+              <Form.Label style={{ color: "white" }}>Message:</Form.Label>
+              <Form.Control
+                value={Message}
+                name="message"
+                onChange={handleInputChange}
+                type="text"
+                placeholder="Send me a message!"
+                as="textarea"
+                size="lg"
+                rows={3}
+              />
+            </Form.Group>
+          </Form>
+          <Button
+            type="button"
+            onClick={handleFormSubmit}
+            variant="outline-light"
+          >
+            Submit
+          </Button>
+          {errorMessage && (
+            <div>
+              <p className="errorText">{errorMessage}</p>
+            </div>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Contact;
