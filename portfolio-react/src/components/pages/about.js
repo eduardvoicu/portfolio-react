9267fb8a454f7a724cc1d7d839f6cf42a7ea1970
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-13 08:04:42 -0600

    added info to about.js file

---
+++ portfolio-react/src/components/pages/about.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./About.css";
+
+export default function About() {
+  return (
+    <div class="mainCont">
+      <section class="aboutMe" id="aboutMe">
+        <h1>About Me</h1>
+        <p>
+        Thanks for checking out my portfolio! My name is Eduard, and I am an aspiring full-stack web/app developer. 
+        Currently, I am a student at Northwestern University in a 6-month cohort program to earn a certificate in Full Stack Web Development. 
+        With a deep background as a program manager, a soon-to-be US Navy veteran, and branding/marketing experience on top of web development, 
+        I intend to use all skills together to help my future clients exceed their business goals. 
+        </p>
+      </section>
+    </div>
+  );
+}
