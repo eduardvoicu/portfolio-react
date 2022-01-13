034a46d718a022b19d55469b1c4b3aa85bdc404f
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-13 08:13:19 -0600

    added home.js file and export function

---
+++ portfolio-react/src/components/pages/home/home.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Home.css";
+import Avatar from "./assets/IMG_8160c.jpg";
+
+export default function Home() {
+  return (
+    <div class="alignAvatar">
+      <img class="avatar" src={Avatar} />
+    </div>
+  );
+}
