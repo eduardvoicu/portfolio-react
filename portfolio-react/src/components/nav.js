1721c22e42c583452138b4a4e19940090fe1ec6d
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-12 21:23:08 -0600

    created nav.js file, added some css style

---
+++ portfolio-react/src/components/nav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Portfolio.css";
+import Resume from "./NEW-Resume.pdf"
+
+const styles = {
+  card: {
+    display: "flex",
+    justifyContent: "center",
+    margin: "25px 0 25px 0",
+    fontSize: "20px",
+  },
+  myName: {
+    color: "rgb(43, 43, 43)",
+    fontSize: "50px",
+    textAlign: "center",
+    marginTop: "25px",
+  },
+};
