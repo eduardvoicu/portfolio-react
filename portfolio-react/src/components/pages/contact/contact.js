2e1d7c6b2d31b3e60906e9f8637d3b9fd032c7a0
Eduard Voicu <iameduardvoicu@gmail.com>
2022-01-13 08:10:15 -0600

    more functions on input added to contact.js

--- portfolio-react/src/components/pages/contact/contact.js
+++ portfolio-react/src/components/pages/contact/contact.js
@@ -8,3 +8,41 @@ function Contact() {
   const [FullName, setFullName] = useState("");
   const [Message, setMessage] = useState("");
   const [errorMessage, setErrorMessage] = useState("");
+
+  const handleInputChange = (e) => {
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    if (inputType === "email") {
+      setEmail(inputValue);
+    } else if (inputType === "FullName") {
+      setFullName(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    e.preventDefault();
+
+    if (!validateEmail(email)) {
+      setErrorMessage("Email is invalid");
+      return;
+    }
+    if (!FullName) {
+      setErrorMessage("Name is required");
+
+      return;
+    }
+    if (!Message) {
+      setErrorMessage("Message is required");
+
+      return;
+    }
+    alert(`${FullName}, thanks for send me a message!`);
+
+    setFullName("");
+    setMessage("");
+    setEmail("");
+  };
